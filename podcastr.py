970b1a1c13d7aaf9754dad937fb6fc30128bfc18
Daniel Sanchez <jdaniel.saed@gmail.com>
2018-03-11 20:43:31 -0600

    add stupid CORS headers

--- podcastr.py
+++ podcastr.py
@@ -4,18 +4,27 @@ import xmltodict
 
 def lambda_handler(event, context):
   # print('event: ', event)
-  rss_url = get_rss(event)
+  rss_url = get_rss_url(event)
   feed = requests.get(rss_url)
   parsed = xmltodict.parse(feed.text)
   rss_as_json = json.dumps(parsed)
-  response = { 'statusCode': 200, 'body': rss_as_json }
+  response = {
+    'headers': headers(),
+    'body': rss_as_json,
+    'statusCode': 200
+  }
   return response
 
-def get_rss(event):
+def get_rss_url(event):
   query_params = event['queryStringParameters']
   print('queryStringParameters: ', query_params)
   rss_url = query_params['rss_url']
   return rss_url
+
+def headers():
+  return {
+    'Access-Control-Allow-Origin' : '*', # Required for CORS support to work
+  }
 
 # with open('result.json', 'w') as f:
 #     print(result, file=f)  # Python 3.x
